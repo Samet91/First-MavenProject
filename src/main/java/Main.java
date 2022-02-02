0d96f6241fb203a47333c149350101811b40f956
Samet Uslu <samet_u@hotmail.de>
2022-02-02 14:49:30 +0100

    First MavenProject

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(check("Hi"));
+        System.out.println(check("Hallo ziemlich langer String"));
+        System.out.println(check("Hallo ziemlich langer fancy String"));
+    }
+
+    public static boolean check(String s) {
+        return s.length() > 20 && s.contains("fancy");
+    }
+}
