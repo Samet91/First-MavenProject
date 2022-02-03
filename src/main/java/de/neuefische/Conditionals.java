b8599ddef4b19ea12b7766594e025fc80105e639
Samet Uslu <samet_u@hotmail.de>
2022-02-03 15:36:00 +0100

    create switch case

--- src/main/java/de/neuefische/Conditionals.java
+++ src/main/java/de/neuefische/Conditionals.java
@@ -2,8 +2,21 @@ package de.neuefische;
 
 public class Conditionals {
 
-    public static String peopleCheck(int numberPeople) {
-        if (numberPeople > 30) {
+    public static String peopleCheck(int numberPeople, String alarmLevel) {
+        switch (alarmLevel) {
+            case "rot":
+                return checkPersons(numberPeople, 0);
+            case "gelb":
+                return checkPersons(numberPeople, 30);
+            case "grün":
+                return checkPersons(numberPeople, 60);
+            default:
+                return "unknown Alarm";
+        }
+    }
+
+    private static String checkPersons(int numberPeople, int maxNumberPeople) {
+        if (numberPeople > maxNumberPeople) {
             return "zu viele Personen";
         }
         return "Maximale Personenanzahl nicht überschritten";
--- src/test/java/de/neuefische/ConditionalsTest.java
+++ src/test/java/de/neuefische/ConditionalsTest.java
@@ -2,19 +2,17 @@ package de.neuefische;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConditionalsTest {
 
     @Test
     void shouldWarnTooManyPeople() {
-        assertEquals("zu viele Personen", Conditionals.peopleCheck(31));
+        assertEquals("zu viele Personen", Conditionals.peopleCheck(31, "gelb"));
     }
 
     @Test
     void shouldNotWarnLess30People() {
-        assertEquals("Maximale Personenanzahl nicht überschritten", Conditionals.peopleCheck(19));
+        assertEquals("Maximale Personenanzahl nicht überschritten", Conditionals.peopleCheck(19, "gelb"));
     }
 }
